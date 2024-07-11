b260b2be70366f8d76a92e7d634bfc599c641c1e
masiyi <masiyi@163163@163.com>
2024-07-11 19:49:22 +0800

    丰富其他方法

--- src/test/java/com/masiyi/ffmpegspringbootstarterdemo/FfmpegSpringBootStarterDemoApplicationTests.java
+++ src/test/java/com/masiyi/ffmpegspringbootstarterdemo/FfmpegSpringBootStarterDemoApplicationTests.java
@@ -19,4 +19,20 @@ class FfmpegSpringBootStarterDemoApplicationTests {
     void extractAudio() {
         System.out.println(ffmpegTemplate.extractAudio("D:\\video.mp4"));
     }
+
+    @Test
+    void copy() {
+        ffmpegTemplate.copy("D:\\video.mp4","D:\\video.avi");
+    }
+    /**
+     * 提取音频、字幕
+     */
+    @Test
+    void extract() {
+        ffmpegTemplate.extract("D:\\video.mp4","D:\\video.aac");
+    }
+    @Test
+    void captureVideoFootage() {
+        ffmpegTemplate.captureVideoFootage("D:\\video.mp4","D:\\cut.mp4","00:01:01","00:01:12");
+    }
 }
